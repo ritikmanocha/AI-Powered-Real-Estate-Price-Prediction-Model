5da39d4f00b3ac59dbf8996dce8c647d38c80ec5
Ritik <ritik2005manocha19@gmail.com>
2026-03-22 23:54:15 +0530

    Update prediction_page.py

--- pages/prediction_page.py
+++ pages/prediction_page.py
@@ -1,9 +1,11 @@
-
-
-
 import streamlit as st
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+# check payment
+if "paid" not in st.session_state:
+    st.warning("Please complete payment first 💳")
+    st.stop()
 
 st.title("House Price Prediction")
 
@@ -12,25 +14,20 @@ bed = st.number_input("Bedrooms")
 bath = st.number_input("Bathrooms")
 age = st.number_input("Age")
 
-# Dummy model
 model = LinearRegression()
 
 X = np.array([
-[1000,2,1,10],
-[1500,3,2,8],
-[2000,3,2,6],
-[2500,4,3,5],
-[3000,4,3,4]
+    [1000,2,1,10],
+    [1500,3,2,8],
+    [2000,3,2,6],
+    [2500,4,3,5],
+    [3000,4,3,4]
 ])
 
 y = np.array([200000,300000,400000,500000,600000])
 
 model.fit(X,y)
 
-if "paid" not in st.session_state:
-    st.error("Please complete payment first")
-    st.stop()
-    
 if st.button("Predict Price"):
     pred = model.predict([[area,bed,bath,age]])
     st.success(f"Estimated Price: ${int(pred[0])}")
